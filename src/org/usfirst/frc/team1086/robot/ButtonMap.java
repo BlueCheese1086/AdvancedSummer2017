b780b4f08b9776487d55e88945ad68a0d203f5a8
Jack McDowell <kevin.ngo0407@gmail.com>
2017-10-15 10:39:47 -0400

    InputManager and ButtonMap from Brie's code

---
+++ src/org/usfirst/frc/team1086/robot/ButtonMap.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team1086.robot;
+
+public class ButtonMap {
+    //LEFT JOYSTICK
+    public static final int SAFETY_DRIVE = 1;
+    public static final int TURN_LEFT = 4;
+    public static final int TURN_RIGHT = 5;
+    
+    //RIGHT JOYSTICK
+    public static final int OCTO_SHIFTER = 1;
+    public static final int STRAIGHT_DRIVE = 2;
+    public static final int RAPID_TURN = 4;
+    public static final int GEAR_DRIVE = 5;
+    
+    //AUX JOYSTICK
+    public static final int EVICT = 1;
+    public static final int CLIMB = 3;
+}
